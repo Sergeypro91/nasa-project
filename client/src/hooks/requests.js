01419cab80f6fe6f618ad85cb4a2a3dedd93b8fb
Sergey Shevtsov <Sergeyprostoas@gmail.com>
2022-02-23 15:34:01 +0300

    feat: Lessons 106-111 complete

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -19,32 +19,32 @@ async function httpGetLaunches() {
 
 // Submit given launch data to launch system.
 async function httpSubmitLaunch(launch) {
-  // try {
-  //   return await fetch(`${API_URL}/launches`, {
-  //     method: 'post',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //     body: JSON.stringify(launch),
-  //   });
-  // } catch (err) {
-  //   return {
-  //     ok: false,
-  //   };
-  // }
+  try {
+    return await fetch(`${API_URL}/launches`, {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(launch),
+    });
+  } catch (err) {
+    return {
+      ok: false,
+    };
+  }
 }
 
 // Delete launch with given ID.
 async function httpAbortLaunch(id) {
-  // try {
-  //   return await fetch(`${API_URL}/launches/${id}`, {
-  //     method: 'delete',
-  //   });
-  // } catch (err) {
-  //   return {
-  //     ok: false,
-  //   };
-  // }
+  try {
+    return await fetch(`${API_URL}/launches/${id}`, {
+      method: 'delete',
+    });
+  } catch (err) {
+    return {
+      ok: false,
+    };
+  }
 }
 
 export { httpGetPlanets, httpGetLaunches, httpSubmitLaunch, httpAbortLaunch };
--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,20 +1,48 @@
 const launches = new Map();
 
+let latestFlightNumber = 100;
+
 const launch = {
   flightNumber: 100,
   mission: 'Kepler exploration X',
   rocket: ' Explorere IS1',
   launchDate: new Date('Decemver 27, 2030'),
-  destination: 'Kepler-444 A b',
-  customer: ['ZTM', 'NASA'],
+  target: 'Kepler-444 A b',
+  customers: ['ZTM', 'NASA'],
   upcoming: true,
   success: true,
 };
 
 launches.set(launch.flightNumber, launch);
 
+function existLaunchWithId(launchId) {
+  return launches.has(launchId);
+}
+
 function getAllLaunches() {
   return Array.from(launches.values());
 }
 
-module.exports = { getAllLaunches };
+function addNewLaunch(launch) {
+  latestFlightNumber++;
+  launches.set(
+    latestFlightNumber,
+    Object.assign(launch, {
+      success: true,
+      upcoming: true,
+      customers: ['Zero to Mastery', 'NASA'],
+      flightNumber: latestFlightNumber,
+    }),
+  );
+}
+
+function abortLaunchById(launchId) {
+  const aborted = launches.get(launchId);
+
+  aborted.upcoming = false;
+  aborted.success = false;
+
+  return aborted;
+}
+
+module.exports = { getAllLaunches, addNewLaunch, existLaunchWithId, abortLaunchById };
