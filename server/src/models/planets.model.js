4a5131416e9fe56fdb8da68f3fea59792d81b473
Sergey Shevtsov <Sergeyprostoas@gmail.com>
2022-03-07 17:58:15 +0300

    feat: Lessons 130-150 complete

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -2,14 +2,14 @@ const fs = require('fs');
 const path = require('path');
 const { parse } = require('csv-parse');
 
-const habitablePlanets = [];
+const planets = require('./planets.mongo');
 
 function isHabitablePlanet(planet) {
   return (
     planet['koi_disposition'] === 'CONFIRMED' &&
     planet['koi_insol'] > 0.36 &&
-    planet['koi_insol'] > 1.11 &&
-    planet['koi_prad'] < 0.6
+    planet['koi_insol'] < 1.11 &&
+    planet['koi_prad'] < 1.6
   );
 }
 
@@ -17,9 +17,9 @@ function loadPlanetsData() {
   return new Promise((resolve, reject) => {
     fs.createReadStream(path.join(__dirname, '..', '..', 'data', 'kepler_data.csv'))
       .pipe(parse({ comment: '#', columns: true }))
-      .on('data', (data) => {
+      .on('data', async (data) => {
         if (isHabitablePlanet(data)) {
-          habitablePlanets.push(data);
+          savePlanet(data);
         }
       })
       .on('error', (err) => {
@@ -27,16 +27,25 @@ function loadPlanetsData() {
 
         reject(err);
       })
-      .on('end', () => {
-        console.log(`${habitablePlanets.length} - hibitable planets found!`);
+      .on('end', async () => {
+        const countPlanetsFound = (await getAllPlanets()).length;
+        console.log(`${countPlanetsFound} - habitable planets found!`);
 
         resolve();
       });
   });
 }
 
-function getAllPlanets() {
-  return habitablePlanets;
+async function getAllPlanets() {
+  return await planets.find({});
+}
+
+async function savePlanet(planet) {
+  try {
+    await planets.updateOne({ keplerName: planet.kepler_name }, { keplerName: planet.kepler_name }, { upsert: true });
+  } catch (err) {
+    console.error(`Could not save planet ${err}`);
+  }
 }
 
 module.exports = { loadPlanetsData, getAllPlanets };
