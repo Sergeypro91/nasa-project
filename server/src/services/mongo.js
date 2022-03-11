0e90d022955d8f1a9ed7e752f44636555f25f356
Sergey Shevtsov <Sergeyprostoas@gmail.com>
2022-03-11 11:35:22 +0300

    feat: Chapter 13 / Lessons 16-18 complete

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGO_URL =
-  'mongodb+srv://Sergeyprostoas:Prostoas19OG1991@clusternotes.vfxbw.mongodb.net/nasa?retryWrites=true&w=majority';
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connection.once('open', () => {
   console.log('MongoDB connection ready!');
